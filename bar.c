7e399ed195abe8f80af69a449fe848f350c2a7cb
Md.Rafiuzzaman Khan <ponickid@gmail.com>
2020-08-10 13:52:30 +0600

    bar

---
+++ bar.c
@@ -0,0 +1,15 @@
+#include <graphics.h>
+#include <conio.h>
+
+main()
+{
+   int gd = DETECT, gm;
+
+   initgraph(&gd, &gm, "");
+
+   bar(100, 100, 200, 200);
+
+   getch();
+   closegraph();
+   return 0;
+}
